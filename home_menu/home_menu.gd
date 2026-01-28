9c37b988fddd050ea5167338240324a27f550201
Federico Cara <fcara.utn@gmail.com>
2026-01-28 00:57:12 -0300

    Imported heartslayer assets. Started working on gamee

--- home_menu/home_menu.gd
+++ home_menu/home_menu.gd
@@ -1,6 +1,11 @@
 extends Control
 
 const CREDITS := "res://credits/credits.tscn"
+const GAME := "res://game/game.tscn"
 
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file(CREDITS)
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file(GAME)
